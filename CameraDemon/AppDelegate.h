c14b720bc2bf116401b329ac4c9a8fadcc5c0600
七度雨 <269656131@qq.com>
2016-05-31 16:36:17 +0800

    Initial Commit

---
+++ CameraDemon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraDemon
+//
+//  Created by SevenCelsius on 16/5/31.
+//  Copyright © 2016年 SevenCelsius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
